660e8b734ea74eb6112c7f459983b8c134b8e3fe
Thomas Daly <54075922+thomdaly@users.noreply.github.com>
2019-11-20 08:34:41 -0500

    Add files via upload

--- inlineMath.js
+++ inlineMath.js
@@ -65,29 +65,40 @@ const changeArray = [
 
 const replaceArray = [
     ["TO", (passedString) => {
-        return passedString.sup();
+        return "<span class='TO' style='display: inline-block; position: relative; transform: translateY(-0.4em); font-size: 0.8em; padding-left: 0.15em;'>" + passedString + "</span>";
     }],
     ["SUB", (passedString) => {
-        return passedString.sub();
+        return "<span class='SUB' style='display: inline-block; position: relative; transform: translateY(0.4em); font-size: 0.8em; padding-left: 0.15em;'>" + passedString + "</span>";
     }],
     ["OVER", (passedString) => {
-        return "<div style='display: inline-block; position: relative; vertical-align: middle; text-align: center; font-size: 0.8em; margin: 0em 0.4em 0em 0.4em; transform: translateY(-0.1em);'><span style='display: block;'>" + passedString.split(", ")[0] + "</span><span style='display: none'>/</span><span style='border-top: thin solid black; display: block;'>" + passedString.split(", ")[1] + "&nbsp</span></div>";
+        return "<span class='OVER'><div style='display: inline-block; position: relative; vertical-align: middle; text-align: center; font-size: 0.8em; margin: 0em 0.4em 0em 0.4em; transform: translateY(-0.1em);'><span style='display: block;'>" + passedString.split(", ")[0] + "</span><span style='display: none'>/</span><span style='border-top: 0.04em solid black; display: block;'>" + passedString.split(", ")[1] + "&nbsp</span></div></span>";
     }],
     ["SQRT", (passedString) => {
-        return "√<span style='font-size: 0.75em; border-top: solid 0.05em black; margin-left: 0.2em; padding-right: 0.5em;'>" + passedString + "</span>";
-    }],
-    ["ROOT", (passedString) => {
-        return "<span style='display: inline-block; font-size: 0.5em; transform: translate(0.6em, -0.8em); text-align: right;'>" + passedString.split(", ")[0] + "</span>√<span style='font-size: 0.75em; border-top: solid 0.05em black; margin-left: 0.2em; padding-right: 0.5em;'>" + passedString.split(", ")[1] + "</span>";
+        return "<span class='SQRT'><span style='font-size: 1.1em;'>√</span><span style='font-size: 0.8em; border-top: solid 0.05em black; margin-left: 0.14em; padding-right: 0.5em; padding-top: 0.02em;'>" + passedString + "</span></span>";
     }],
 ]
 
 for (var i = 0; i < mathTag.length; i++) {
     mathTag[i].style.fontFamily = "computerModern";
-    mathTag[i].style.margin = "0 0.3em 0 0.3em";
+    mathTag[i].style.cursor = "pointer";
+    mathTag[i].style.padding = "0 0.3em 0 0.3em";
+    mathTag[i].style.borderRadius = "0.4em";
+
+    var alert = document.createElement("div");
+    alert.style.display = "block";
+    alert.style.position = "absolute";
+    alert.style.textAlign = "center";
+    alert.style.borderRadius = "0.4em";
+    alert.style.padding = "0 0.3em 0 0.3em";
+    alert.style.fontSize = "0.7em";
+    alert.style.opacity = "0";
+    alert.innerHTML = "Item successfully copied to clipboard";
+    alert.className = "alert";
+    mathTag[i].append(alert);
 
     for (var j = 0; j < replaceArray.length; j++) {
         replaceArray[j][0] += "{";
-        for (var l; mathTag[i].innerHTML.split(replaceArray[j][0]).length != 1; l) {
+        while (mathTag[i].innerHTML.split(replaceArray[j][0]).length != 1) {
             var newStr = "";
             var iter = 0;
             for (var n = replaceArray[j][0].length; mathTag[i].innerHTML[mathTag[i].innerHTML.indexOf(replaceArray[j][0]) + n] != "}" || iter > 0; n++) {
@@ -100,17 +111,71 @@ for (var i = 0; i < mathTag.length; i++) {
     }
 
     for (var j = 0; j < changeArray.length; j++) {
-        for (var l; mathTag[i].innerHTML.split(changeArray[j][0]).length != 1; l) {
+        while (mathTag[i].innerHTML.split(changeArray[j][0]).length != 1) {
             mathTag[i].innerHTML = mathTag[i].innerHTML.substr(0, mathTag[i].innerHTML.indexOf(changeArray[j][0])) + changeArray[j][1] + mathTag[i].innerHTML.substr(mathTag[i].innerHTML.indexOf(changeArray[j][0]) + changeArray[j][0].length)
         }
     }
 
-    for (var j = 0; j < mathTag[i].children.length; j++) {
-        if (mathTag[i].children[j].tagName == "SUP") {
-            mathTag[i].children[j].style.fontSize = "0.6em";
-            mathTag[i].children[j].style.marginLeft = "0.1em";
-        } else if (mathTag[i].children[j].tagName == "SUB") {
-            mathTag[i].children[j].style.fontSize = "0.5em";
+    mathTag[i].addEventListener("click", (e) => {
+        for (var i = 0; i < mathTag.length; i++) {
+            if (mathTag[i] == e.target) {
+                var obj = e.target;
+            } else {
+                var obj = e.target.closest("math-inline");
+            }
         }
-    }
+
+        var dupeObj = obj.cloneNode(true);
+        var alertObj = null;
+        for (var i = 0; i < obj.children.length; i++) {
+            if (obj.children[i].className == "alert") {
+                alertObj = obj.children[i];
+                break;
+            }
+        }
+
+        alertObj.style.transform = "translateY(-2em)";
+        var u2 = 0;
+        var interval2 = setInterval(() => {
+            u2 >= 1.0 ? clearInterval(interval2) : null;
+            alertObj.style.setProperty("opacity", "" + ((0 - u2) * 0 + u2 * 1) + "");
+            alertObj.style.setProperty("transform", "translateY(" + ((0 - u2) * 0 + u2 * 1) + "em)");
+            u2 += 1 / 30;
+        }, 10);
+        setTimeout(() => {
+            var u2 = 0;
+            var interval2 = setInterval(() => {
+                u2 >= 1.0 ? clearInterval(interval2) : null;
+                alertObj.style.setProperty("opacity", "" + ((1 - u2) * 1 + u2 * 0) + "");
+                alertObj.style.setProperty("transform", "translateY(" + ((1 - u2) * 1 + u2 * 0) + "em)");
+                u2 += 1 / 30;
+            }, 10);
+        }, 1200)
+
+        var rewrite = {
+            TO: () => {
+                dupeObj.innerHTML = dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[0] + "^(" + dupeObj.children[0].innerHTML + ")" + dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[1];
+            },
+            SUB: () => {
+                dupeObj.innerHTML = dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[0] + "sub(" + dupeObj.children[0].innerHTML + ")" + dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[1];
+            },
+            OVER: () => {
+                dupeObj.innerHTML = dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[0] + "((" + dupeObj.children[0].children[0].children[0].innerHTML + ") / (" + dupeObj.children[0].children[0].children[2].innerHTML.replace('&nbsp;', '') + "))" + dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[1];
+            },
+            SQRT: () => {
+                dupeObj.innerHTML = dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[0] + "sqrt(" + dupeObj.children[0].children[1].innerHTML + ")" + dupeObj.innerHTML.split(dupeObj.children[0].outerHTML)[1];
+            },
+        }
+
+        while (dupeObj.innerHTML.split("<span").length != 1) {
+            rewrite[dupeObj.children[0].className]();
+        }
+
+        var el = document.createElement('textarea');
+        el.value = dupeObj.innerHTML.split("<div")[0];
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    });
 }
